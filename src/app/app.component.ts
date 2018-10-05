9d37567907143db6e4a7e3f49e2a69096ed95816
mrtruebeliever <mrtruebeliever@gmail.com>
2018-10-05 13:51:25 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'zvo-genesis';
+  data = {
+    'Apple': null,
+    'Microsoft': null,
+    'Google': null
+  };
+
+
+  // autocomplete
+  autocompleteOptions: Materialize.AutoCompleteOptions = {
+    data: {
+      'Apple': null,
+      'Microsoft': null,
+      'Google': 'assets/google_g_logo.png',
+    },
+  };
+
+  ngOnInit(){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MzChipModule } from 'ngx-materialize'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MzChipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
